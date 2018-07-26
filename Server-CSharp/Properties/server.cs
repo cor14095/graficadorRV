4f750378edfc681fe6749fbfe78539d9edbaa06f
cor14095 <ale.crts1996@gmail.com>
2018-07-26 01:32:45 -0500

    Clear server.cs trash code from previous project.

--- Server-CSharp/Properties/server.cs
+++ Server-CSharp/Properties/server.cs
@@ -128,40 +128,5 @@ namespace ConsoleApplication1
             }
            
         }
-        /*
-        private void doChat()
-        {
-            int requestCount = 0;
-            byte[] bytesFrom = new byte[10025];
-            string dataFromClient = null;
-            Byte[] sendBytes = null;
-            string serverResponse = null;
-            string rCount = null;
-            requestCount = 0;
-
-            while ((true))
-            {
-                try
-                {
-                    requestCount = requestCount + 1;
-                    NetworkStream networkStream = clientSocket.GetStream();
-                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
-                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
-                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
-                    Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
-
-                    rCount = Convert.ToString(requestCount);
-                    serverResponse = "Server to clinet(" + clNo + ") " + rCount;
-                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
-                    networkStream.Write(sendBytes, 0, sendBytes.Length);
-                    networkStream.Flush();
-                    Console.WriteLine(" >> " + serverResponse);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(" >> " + ex.ToString());
-                }
-            }
-        }*/
     } 
 }
